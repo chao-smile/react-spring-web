4dec16e4deee097ddc6ae16eead1c74b5eb4df2a
chao-smile <chaosmile404@gmail.com>
2024-04-09 15:48:48 +0800

    chore: 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": `${__dirname}/src`,
+      "@components": `${__dirname}/src/components`,
+      "@utils": `${__dirname}/src/utils`,
+    },
+  },
+});
